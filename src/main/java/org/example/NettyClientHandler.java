cbaade1ed3aa2fa8554f842222a35238bb5d4635
moshenglei <moshenglei@zhongan.com>
2019-12-20 16:35:31 +0800

    a

--- src/main/java/org/example/NettyClientHandler.java
+++ src/main/java/org/example/NettyClientHandler.java
@@ -8,8 +8,27 @@ import io.netty.util.CharsetUtil;
 
 public class NettyClientHandler extends ChannelInboundHandlerAdapter {
 
+    /**
+     * 当客户端连接服务器完成就会触发该方法
+     * @param ctx
+     * @throws Exception
+     */
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
         ByteBuf buf = Unpooled.copiedBuffer("Hello Server", CharsetUtil.UTF_8);
         ctx.writeAndFlush(buf);
+    }
+
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf)msg;
+        System.out.println("收到服务端的消息： " + buf.toString(CharsetUtil.UTF_8));
+        System.out.println("服务端的地址：" + ctx.channel().remoteAddress());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
     }
 }
--- src/main/java/org/example/NettyServerHandler.java
+++ src/main/java/org/example/NettyServerHandler.java
@@ -1,6 +1,35 @@
 package org.example;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
 
 public class NettyServerHandler extends ChannelInboundHandlerAdapter {
+
+    /**
+     *
+     * 读取客户端发送的数据
+     * @param ctx
+     * @param msg
+     * @throws Exception
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("服务器读取线程 " + Thread.currentThread().getName());
+        ByteBuf buf = (ByteBuf)msg;
+        System.out.println("客户端发送的消息是: " + buf.toString(CharsetUtil.UTF_8));
+    }
+
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        super.channelReadComplete(ctx);
+    }
+
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        super.exceptionCaught(ctx, cause);
+    }
 }
